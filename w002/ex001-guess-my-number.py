8fadd59083244c58663556a373c1d26926e06132
Joby Morey <nomadfarmer@gmail.com>
2018-06-26 14:22:51 -0400

    added first coding exercise from week 2

---
+++ w002/ex001-guess-my-number.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Guess my number exercise from the edX MIT 6.00.1x intro to cs course
+Author: Joby/nomadfarmer
+"""
+
+upperLimit = 100
+lowerLimit = 0
+
+"""
+Strings:
+Please think of a number between 0 and 100!
+Is your secret number <guess>?
+Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.
+Game over. Your secret number was: <guess>
+"""
