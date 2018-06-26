f0f47aa200c3c75a39dac0170e9cef8ebcd952d0
Joby Morey <nomadfarmer@gmail.com>
2018-06-26 12:41:14 -0400

    First. Added one file from chapter 1.

---
+++ ch001/hw003-longest-ordered-string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 17:29:00 2018
+
+@author: joby
+"""
+
+s = 'azcbobobegghaklbob'
+
+longest = s[0]
+current = ''
+
+for start in range(len(s)):
+    current = s[start]
+    while len(current) + start < len(s) and s[start+len(current)] >= current[-1]:
+        current += s[start+len(current)]
+    if len(current) > len(longest):
+        longest = current
+print ("Longest substring in alphabetical order is: " + longest)
