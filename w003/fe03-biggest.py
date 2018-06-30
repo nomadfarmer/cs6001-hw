5e9f4e676f8821bc823bfae6d17b260cd3896f7b
Joby Morey <nomadfarmer@gmail.com>
2018-06-30 03:16:36 -0400

    'Biggest' FE, plus 3/4 through pset for week 3

--- w003/fe03-biggest.py
+++ w003/fe03-biggest.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
-Function how_many should take a dictionary as an argument. Each value
-in the dictionary should be a list, according to the sketchy specs. Our
-function should return the total number of elements of all of those
-lists.
+Function biggest should take a dictionary as an argument. Each value
+in the dictionary should be a list. Our function should return key which
+has the most values associated with it -- the longest list.
 """
 
 __author__ = "Joby/Nomadfarmer"
@@ -14,18 +13,20 @@ __license__ = "MIT"
 def main():
     d = {'b': [1], 'c': [2, 3, 4], 'd': [5, 6]}
 
-    if how_many(d) == 6:
+    if biggest(d) == 'c':
         print("good")
     else:
         print("bad")
 
 
-def how_many(a_dict):
-    count = 0
-    for l in a_dict.values():
-        print(l)
-        count += len(l)
-    return count
+def biggest(a_dict):
+    longest_key = None
+    longest_length = 0
+    for k in a_dict.keys():
+        if len(a_dict[k]) > longest_length:
+            longest_length = len(a_dict[k])
+            longest_key = k
+    return longest_key
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
